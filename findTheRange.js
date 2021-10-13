924ebcb03913d4a3e9e765a28286455d7fc0e749
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-10-13 09:02:55 -0600

    Initial

---
+++ findTheRange.js
@@ -0,0 +1,10 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+let min = Math.min(array);
+    console.log(min);
+
+let max = Math.max(array);
+    console.log(max)
+
+// Write your solution below:
